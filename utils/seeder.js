8679b00be01a35d54ec8fa6cf74a8de48b6aea6f
colburncodes <colburnsanders@gmail.com>
2021-12-08 21:39:37 -0600

    seed data, crud for room

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,5 +1,21 @@
-import mongoose from 'mongoose'
-import Room from '../models/room'
 
-import room from '../data/rooms'
+import dbConnect from "../config";
 
+import Room from "../models/room";
+import rooms from "../data/rooms";
+
+dbConnect();
+
+const seedRooms = async () => {
+  try {
+    await Room.deleteMany(); //remove room collection
+    console.log("Rooms are deleted");
+    await Room.insertMany(rooms);
+    console.log("All rooms are added.");
+  } catch (error) {
+    console.error(error.message);
+    process.exit();
+  }
+};
+
+seedRooms();
